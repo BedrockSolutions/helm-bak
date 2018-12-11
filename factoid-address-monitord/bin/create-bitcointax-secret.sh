8fa50e64fd32dfd43e94064e8f29a1ff77d7070f
Jay Cheroske <jay@portal.org>
2018-12-10 18:13:28 -0800

    fam changes

--- factoid-address-monitord/bin/create-bitcointax-secret.sh
+++ factoid-address-monitord/bin/create-bitcointax-secret.sh
@@ -3,16 +3,13 @@
 set -e
 
 while [[ "$#" > 0 ]]; do case $1 in
-  --key) key="${2}"; shift;;
   --secret) secret="${2}"; shift;;
-  -h|--help) echo "${0} --key <key> --secret <secret>"; exit 0;;
+  -h|--help) echo "${0} --secret <secret>"; exit 0;;
   *) echo "Unknown parameter passed: $1"; exit 1;;
 esac; shift; done
 
-if [ -z "key" ]; then echo "--key is required" && exit 1; fi
 if [ -z "secret" ]; then echo "--secret is required" && exit 1; fi
 
 kubectl create secret generic \
   factoid-address-monitord-secret \
-  --from-literal="bitcoinTaxKey=${key}" \
   --from-literal="bitcoinTaxSecret=${secret}"
